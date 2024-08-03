c6da9904a43295afd6c4f51547c409e58656880d
Eddy Xiao <bewantbe@gmail.com>
2024-08-04 06:53:26 +0800

    Add function CABLE.

--- main.py
+++ main.py
@@ -14,9 +14,6 @@ from tqdm import tqdm
 
 import dataLoader
 
-# directions of "gradient field". lmax=8 <=> 45 directions
-dirs = cp.loadtxt("./45.txt")[1:,:-1].astype(np.float32)
-
 def split(data, pad, block, roi = None):
     """
     @param
@@ -72,7 +69,7 @@ def grad(temp):
                                 mode='nearest', truncate=4)[::ratio, ::ratio,::ratio]
     return Vx, Vy, Vz
 
-def psf(gradient, index):
+def psf(gradient, field_dirs, index):
     """
     For each gradient of the image, project it to predefined directions and adjust it 
     so that only the aligned direction has a large value.
@@ -81,30 +78,38 @@ def psf(gradient, index):
     """
     gradient_norm = np.linalg.norm(gradient, axis=-1)[...,None]
     gradient = np.nan_to_num(gradient / gradient_norm)
-    o = gradient @ dirs.T
+    o = gradient @ field_dirs.T
     return o ** index * gradient_norm
 
 sigma = 1
 rho   = 20    # size of analyze step (also control analyze window and block size)
 ratio = 2     # downsample ratio, i.e. step size for gradient computaion
 
-def main():
-    #address = sys.argv[1]
-    address = '/mnt/data_ext/projects/cable/CJ4TEST.h5'
-    h5 = h5py.File(address, 'r')
+def ComputeGradientDiffusionImage(img3d_path, dwi_path):
+    # load 3D image
+    h5 = h5py.File(img3d_path, 'r')
     img = h5['DataSet']['ResolutionLevel 2']['TimePoint 0']['Channel 1']['Data']
     ims_roi = [0, img.shape[0],
                0, img.shape[1],
                0, img.shape[2]]
+    #ims_roi = [0, 200,
+    #           500, 800,
+    #           500, 800]
     nii_roi = [img.shape[2]-ims_roi[5], img.shape[2]-ims_roi[4],
                img.shape[0]-ims_roi[1], img.shape[0]-ims_roi[0],
                img.shape[1]-ims_roi[3], img.shape[1]-ims_roi[2]]
+    print(f'Image ROI: {nii_roi}')
     ODF_Data = np.zeros([(nii_roi[1]-nii_roi[0]) // rho,
                          (nii_roi[3]-nii_roi[2]) // rho,
                          (nii_roi[5]-nii_roi[4]) // rho, 46], dtype=np.float32)
+    print(f'Result image size: {ODF_Data.shape}')
+
+    # directions of "gradient field". lmax=8 <=> 45 directions
+    field_dirs = cp.loadtxt("./45.txt")[1:,:-1].astype(np.float32)
+
     padded_index, effective_index, ODF_Index = split(
         img, 1 * rho, [10 * rho, 10 *  rho, 10 * rho], nii_roi)
-    dataSet = dataLoader.CustomIMSDataset(padded_index, effective_index, ODF_Index, address)
+    dataSet = dataLoader.CustomIMSDataset(padded_index, effective_index, ODF_Index, img3d_path)
     dataloader = dataLoader.DataLoader(dataSet, batch_size=1, num_workers=10, pin_memory=True)
     ker1d = torch.tensor(cv2.getGaussianKernel(
             rho//ratio + 1, rho//ratio*10, cv2.CV_32F)[:, 0]).cuda(0)
@@ -121,7 +126,7 @@ def main():
         Vz[flag] = 0
         r = cp.stack((Vx, Vy, Vz), axis=-1)
         # get simulated directional response
-        x = psf(r, 100)
+        x = psf(r, field_dirs, 100)
         # smooth the response and down sampling to resolution `rho`
         x = torch.as_tensor(x[None, ...].transpose([4, 0, 1, 2, 3])).cuda()
         x = F.conv3d(x, ker1d[None, None, None, None, :],
@@ -151,18 +156,7 @@ def main():
         i += 1
 
     out = makeNiiForMRView(rho, ODF_Data)
-    dwi = f'./DWI_sigma_{sigma}_rho_{rho}.nii'
-    res = f'./response.nii'
-    fod = f'./FOD_sigma_{sigma}_rho_{rho}.nii'
-    dir_file = f'./45.txt'
-    nib.save(out, dwi)
-
-    # os.system(f'dwi2response tax {dwi}  {res}  -grad {dir_file}  -force')
-    os.system(f'mrfilter "{dwi}" smooth -stdev 0.06,0.06,0.06 "{dwi}" -force')
-    os.system(f'dwi2fod msmt_csd "{dwi}" "{res}" "{fod}" -grad "{dir_file}" -lmax 12 -shells 1000 -force')
-    os.system(f'dwi2mask "{dwi}" mask.nii -grad "{dir_file}" -force')
-    os.system(f'tckgen -seed_image mask.nii "{fod}" tck.tck -select 10000 -power 2 -cutoff 0.01 -force -minlength 0.01 -maxlength 100000 -force')
-    os.system(f'mrview "{dwi}" -imagevisible false -tractography.load tck.tck')
+    nib.save(out, dwi_path)
 
 def makeNiiForMRView(fod_reslotion, out):
     out = nib.Nifti1Image(out, np.eye(4) * fod_reslotion*4/1000)
@@ -170,6 +164,30 @@ def makeNiiForMRView(fod_reslotion, out):
     out.affine[1, -1] = fod_reslotion / 2
     out.affine[2, -1] = fod_reslotion / 2
     return out
+
+def MRtrixProcess(dwi_path, n_tck_sample):
+    res_path  = f'./response.nii'
+    fod_path  = f'./FOD_sigma_{sigma}_rho_{rho}.nii'
+    dir_path  = f'./45.txt'
+    mask_path = f'mask.nii'
+    tck_path  = f'tck.tck'
+
+    # os.system(f'dwi2response tax {dwi}  {res}  -grad {dir_file}  -force')
+    os.system(f'mrfilter "{dwi_path}" smooth -stdev 0.06,0.06,0.06 "{dwi_path}" -force')
+    os.system(f'dwi2fod msmt_csd "{dwi_path}" "{res_path}" "{fod_path}" -grad "{dir_path}" -lmax 12 -shells 1000 -force')
+    os.system(f'dwi2mask "{dwi_path}" "{mask_path}" -grad "{dir_path}" -force')
+    os.system(f'tckgen -seed_image "{mask_path}" "{fod_path}" "{tck_path}" -select {n_tck_sample} -power 2 -cutoff 0.01 -force -minlength 0.01 -maxlength 100000 -force')
+    os.system(f'mrview "{dwi_path}" -imagevisible false -tractography.load "{tck_path}"')
+
+def ComputeCABLE(img3d_path, n_tck_sample):
+    dwi_path = f'./DWI_sigma_{sigma}_rho_{rho}.nii'
+    ComputeGradientDiffusionImage(img3d_path, dwi_path)
+    MRtrixProcess(dwi_path, n_tck_sample)
+
+def main():
+    img3d_path = sys.argv[1]
+    n_tck_sample = 10000
+    ComputeCABLE(img3d_path, n_tck_sample)
 
 if __name__ == '__main__':
     main()
