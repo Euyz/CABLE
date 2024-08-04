f6813f42d9fdf7da398dc38590021e09c87c0f1e
yue <33593212+Euyz@users.noreply.github.com>
2024-08-04 12:06:21 +0800

    Add files via upload

--- dataLoader.py
+++ dataLoader.py
@@ -4,39 +4,34 @@ import numpy as np
 import h5py
 
 class CustomIMSDataset(Dataset):
-    def __init__(self, pb, rb, tb, data, z0=0):
-        self.pb = pb
-        self.rb = rb
-        self.tb = tb
-        self.data = data
+    def __init__(self, ODF_partition, data, z0=0):
+        self.ODF_partition=ODF_partition
+        self.data=data
         self.open_hdf5()
-
-        self.z0 = z0
-        # mark slice image boundary
-        self.flag = np.zeros(self.data.shape, dtype=bool)
-        self.flag[ 0::75] = True
-        self.flag[ 1::75] = True
-        self.flag[ 2::75] = True
-        self.flag[74::75] = True
-        self.flag[73::75] = True
+        self.z0=z0
+        # self.flag=np.zeros(self.data.shape,dtype=bool)
+        # self.flag[0::75]=True
+        # self.flag[1::75] = True
+        # self.flag[2::75] = True
+        # self.flag[74::75] = True
+        # self.flag[73::75] = True
 
     def getDataByNiiIndexFromTif(self, tif, nii_roi):
-        temp      = self.axisTransform(tif,       nii_roi)
-        temp_flag = self.axisTransform(self.flag, nii_roi)
-        return torch.tensor(temp.astype(np.float32)), temp_flag.copy()
+        temp=self.axisTransform(tif,nii_roi)
 
-    def axisTransform(self, tif, nii_roi):
+        return torch.tensor(temp.astype(np.float32))
+
+    def axisTransform(self,tif,nii_roi):
         shape = tif.shape
-        temp = tif[shape[0] - nii_roi[1][1] : shape[0] - nii_roi[1][0],
-                   shape[1] - nii_roi[2][1] : shape[1] - nii_roi[2][0],
-                   shape[2] - nii_roi[0][1] : shape[2] - nii_roi[0][0]]
+        temp = tif[shape[0] - nii_roi[1][1]: shape[0] - nii_roi[1][0],
+               shape[1] - nii_roi[2][1]:shape[1] - nii_roi[2][0], shape[2] - nii_roi[0][1]:shape[2] - nii_roi[0][0]]
         temp = np.flip(temp, axis=(0, 1, 2))
         temp = np.swapaxes(temp, 0, 1)
         temp = np.swapaxes(temp, 0, 2)
         return temp
 
     def __len__(self):
-        return len(self.pb)
+        return len(self.ODF_partition)
 
     def open_hdf5(self):
         self.img_hdf5 = h5py.File(self.data, 'r')
@@ -45,4 +40,4 @@ class CustomIMSDataset(Dataset):
     def __getitem__(self, i):
         if not hasattr(self, 'img_hdf5'):
             self.open_hdf5()
-        return self.getDataByNiiIndexFromTif(self.data, self.pb[i])
+        return self.getDataByNiiIndexFromTif(self.data, self.ODF_partition[i]),self.ODF_partition[i]
--- main.py
+++ main.py
@@ -1,221 +1,99 @@
-# If you find this code useful in your research, consider cite the following paper:
-# Yue Zhang et al. Whole-brain reconstruction of fiber tracts based on the 3-D cytoarchitectonic organization
-
-import os
-import sys
-
-import numpy as np
-import h5py
-import nibabel as nib
-import cv2
-
 import torch
+import h5py
 import torch.nn.functional as F
+import numpy as np
+import nibabel as nib
+import dataLoader
 import cupyx.scipy.ndimage as filter
 import cupy as cp
+import os
+import sys
 from tqdm import tqdm
+dirs = cp.loadtxt("./45.txt")[1:,:-1].astype(np.float32)
+sigma = 1
+rho = 20
+ratio = 2
+dwi = f'./DWI_sigma_{sigma}_rho_{rho}.nii'
+dwi_denoise = f'./DWI_sigma_{sigma}_rho_{rho}_denoise.nii'
+res = f'./response.nii'
+fod = f'./FOD_sigma_{sigma}_rho_{rho}.nii'
+dir_file = f'./45.txt'
 
-import dataLoader
 
-def split(data, pad, block, roi = None):
-    """
-    @param
-    pad   : padding to block, e.g. 20
-    block : block size, e.g. 200x200x200
-    roi   : e.g. nii_roi [0, 2176, 0, 232, 0, 2112]
-    @return
-    pad_index       : list of blocked ROI with padding around covering the large ROI `roi`,
-                      such as [..., [[0, 220], [180, 232], [0, 220]], ...]
-    effective_index : relative ROI for ODF_Index to pad_index,
-                      such as [..., [[0, 200], [20, 52], [0, 200]], ...]
-    ODF_Index       : list of blocked ROI with no overlap that tile the large ROI,
-                      such as [..., [[0, 200], [200, 232], [0, 200]], ...]
-    """
-    pad = pad
-    block = block
-    shape = data.shape
-    if roi is None:
-        roi = [0, shape[2], 0, shape[0], 0, shape[1]]
-    flag = [[i for i in range(roi[j*2], roi[j*2+1], block[j])]
-            for j in range(3)]
-    for i in range(len(flag)):
-        flag[i].append(roi[i*2+1])
-    pad_index = []
-    effective_index = []
-    ODF_Index = []
-    for i in range(len(flag[0]) -1):
-        for j in range(len(flag[1]) -1):
-            for k in range(len(flag[2])-1 ):
-                xl = max(flag[0][i] - pad, 0)
-                xr = min(flag[0][i + 1] + pad, roi[1])
-                yl = max(flag[1][j] - pad, 0)
-                yr = min(flag[1][j + 1] + pad, roi[3])
-                zl = max(flag[2][k] - pad, 0)
-                zr = min(flag[2][k + 1] + pad, roi[5])
-                ODF_Index.append(
-                    [[flag[0][i], flag[0][i + 1]],
-                     [flag[1][j], flag[1][j + 1]],
-                     [flag[2][k], flag[2][k + 1]]])
-                pad_index.append([[xl, xr], [yl, yr], [zl, zr]])
-                effective_index.append([[flag[0][i] - xl, flag[0][i + 1] - xl],
-                                        [flag[1][j] - yl, flag[1][j + 1] - yl],
-                                        [flag[2][k] - zl, flag[2][k + 1] - zl]])
-    return pad_index, effective_index, ODF_Index
+def partition(block_size, shape, unit):
+    flags = [
+        [i for i in range(shape[2 * j], shape[2 * j + 1] + 1, block_size[j])]
+        for j in range(3)
+    ]
+    for i in range(3):
+        if flags[i][-1] != shape[2 * i + 1]:
+            flags[i].append(shape[2 * i + 1] - (shape[2 * i + 1] % unit))
+    ODF_partition = []
+    for i in range(len(flags[0]) - 1):
+        for j in range(len(flags[1]) - 1):
+            for k in range(len(flags[2]) - 1):
+                ODF_partition.append([
+                    [flags[0][i], flags[0][i + 1]],
+                    [flags[1][j], flags[1][j + 1]],
+                    [flags[2][k], flags[2][k + 1]]
+                ])
+    return ODF_partition
 
-def grad(temp, sigma, ratio):
-    """compute grad of the image, with gaussian filter"""
-    Vx = filter.gaussian_filter(temp, sigma * 1, order=[1, 0, 0],
-                                mode='nearest', truncate=4)[::ratio, ::ratio,::ratio]
-    Vy = filter.gaussian_filter(temp, sigma * 1, order=[0, 1, 0],
-                                mode='nearest', truncate=4)[::ratio, ::ratio,::ratio]
-    Vz = filter.gaussian_filter(temp, sigma * 1, order=[0, 0, 1],
-                                mode='nearest', truncate=4)[::ratio, ::ratio,::ratio]
+
+def grad(temp):
+    Vx = filter.gaussian_filter(temp, sigma * 1, order=[1, 0, 0])[::ratio, ::ratio, ::ratio]
+    Vy = filter.gaussian_filter(temp, sigma * 1, order=[0, 1, 0])[::ratio, ::ratio, ::ratio]
+    Vz = filter.gaussian_filter(temp, sigma * 1, order=[0, 0, 1])[::ratio, ::ratio, ::ratio]
     return Vx, Vy, Vz
-
-def psf(gradient, field_dirs, index):
-    """
-    For each gradient of the image, project it to predefined directions and adjust it 
-    so that only the aligned direction has a large value.
-        (r / ||r|| @ direction) ** index * ||r||
-    Conceptually simulate the convolultion of the gradient with a sharp PSF.
-    """
+def psf(gradient, index):
     gradient_norm = np.linalg.norm(gradient, axis=-1)[...,None]
     gradient = np.nan_to_num(gradient / gradient_norm)
-    o = gradient @ field_dirs.T
+    o = gradient @ dirs.T
     return o ** index * gradient_norm
-
-def GradientWeightedFunction(img3d_path, cable_params, dwi_path):
-    sigma = cable_params['sigma']
-    rho = cable_params['rho']
-    ratio = cable_params['ratio']
-
-    # load 3D image
-    h5 = h5py.File(img3d_path, 'r')
-    img = h5['DataSet']['ResolutionLevel 2']['TimePoint 0']['Channel 1']['Data']
-    ims_roi = [0, img.shape[0],
-               0, img.shape[1],
-               0, img.shape[2]]
-    #ims_roi = [0, 200,
-    #           500, 800,
-    #           500, 800]
-    nii_roi = [img.shape[2]-ims_roi[5], img.shape[2]-ims_roi[4],
-               img.shape[0]-ims_roi[1], img.shape[0]-ims_roi[0],
-               img.shape[1]-ims_roi[3], img.shape[1]-ims_roi[2]]
-    print(f'Image ROI: {nii_roi}')
-    ODF_Data = np.zeros([(nii_roi[1]-nii_roi[0]) // rho,
-                         (nii_roi[3]-nii_roi[2]) // rho,
-                         (nii_roi[5]-nii_roi[4]) // rho, 46], dtype=np.float32)
-    print(f'Result GWF image size: {ODF_Data.shape}')
-
-    # directions of "gradient field". lmax=8 <=> 45 directions
-    field_dirs = cp.loadtxt("./45.txt")[1:,:-1].astype(np.float32)
-
-    padded_index, effective_index, ODF_Index = split(
-        img, 1 * rho, [10 * rho, 10 *  rho, 10 * rho], nii_roi)
-    dataSet = dataLoader.CustomIMSDataset(padded_index, effective_index, ODF_Index, img3d_path)
-    dataloader = dataLoader.DataLoader(dataSet, batch_size=1, num_workers=10, pin_memory=True)
-    ker1d = torch.tensor(cv2.getGaussianKernel(
-            rho//ratio + 1, rho//ratio*10, cv2.CV_32F)[:, 0]).cuda(0)
-    i = 0
-    for batch in tqdm(dataloader, colour = 'GREEN'):
-        # get blocked image
-        roi = cp.asarray(batch[0][0])
-        flag = cp.asarray(batch[1][0])[::ratio, ::ratio,::ratio]
-
-        # get smoothed gradient
-        Vx, Vy, Vz = grad(roi, sigma, ratio)
-        Vx[flag] = 0
-        Vy[flag] = 0
-        Vz[flag] = 0
-        r = cp.stack((Vx, Vy, Vz), axis=-1)
-        # get simulated directional response
-        x = psf(r, field_dirs, 100)
-        # smooth the response and down sampling to resolution `rho`
-        x = torch.as_tensor(x[None, ...].transpose([4, 0, 1, 2, 3])).cuda()
-        x = F.conv3d(x, ker1d[None, None, None, None, :],
-                     padding=(0, 0, ker1d.shape[0] // 2),
-                     stride=[1, 1, rho // ratio])
-        x = F.conv3d(x, ker1d[None, None, None, :, None],
-                     padding=(0, ker1d.shape[0] // 2, 0),
-                     stride=[1, rho // ratio, 1])
-        x = F.conv3d(x, ker1d[None, None, :, None, None],
-                     padding=(ker1d.shape[0] // 2, 0, 0),
-                     stride=[rho // ratio, 1, 1])
-        x = cp.array(x[..., None].transpose(0, -1)[0][0])
-        cp.nan_to_num(x, 0)
-        # add 0-th component
-        x = cp.concatenate([cp.max(x[:], axis=-1)[..., None] * 2, x], axis=-1)
-        x[..., 0] = cp.where(x[..., 0]>0.2, 1, 0)
-        x = x.get()
-        # write the tile (block) to result array
-        odf = ODF_Index[i]
-        eff = effective_index[i]
-        ODF_Data[(odf[0][0]-nii_roi[0]) // rho : (odf[0][1]-nii_roi[0]) // rho,
-                 (odf[1][0]-nii_roi[2]) // rho : (odf[1][1]-nii_roi[2]) // rho,
-                 (odf[2][0]-nii_roi[4]) // rho : (odf[2][1]-nii_roi[4]) // rho] = \
-            x[eff[0][0] // rho : eff[0][1] // rho,
-              eff[1][0] // rho : eff[1][1] // rho,
-              eff[2][0] // rho : eff[2][1] // rho]
-        i += 1
-
-    out = makeNiiForMRView(rho, ODF_Data)
-    nib.save(out, dwi_path)
-
 def makeNiiForMRView(fod_reslotion, out):
     out = nib.Nifti1Image(out, np.eye(4) * fod_reslotion*4/1000)
-    out.affine[0, -1] = fod_reslotion / 2
-    out.affine[1, -1] = fod_reslotion / 2
-    out.affine[2, -1] = fod_reslotion / 2
     return out
-
-def ODFProcessing(dwi_path, cable_params, fod_path, mask_path, tck_path):
-    n_tck_sample = cable_params['n_tck_sample']
-    res_path     = cable_params['response_path']
-    dir_path     = cable_params['dir_path']
-
-    # os.system(f'dwi2response tax {dwi_path}  {res_path}  -grad {dir_path}  -force')
-    os.system(f'mrfilter "{dwi_path}" smooth -stdev 0.06,0.06,0.06 "{dwi_path}" -force')
-    os.system(f'dwi2fod msmt_csd "{dwi_path}" "{res_path}" "{fod_path}" -grad "{dir_path}" '
-              f'-lmax 12 -shells 1000 -force')
-    os.system(f'dwi2mask "{dwi_path}" "{mask_path}" -grad "{dir_path}" -force')
-    os.system(f'tckgen -seed_image "{mask_path}" "{fod_path}" "{tck_path}" -select {n_tck_sample} '
-              f'-power 2 -cutoff 0.01 -force -minlength 0.01 -maxlength 100000 -force')
-
-def ComputeCABLE(img3d_path, cable_params):
-    sigma = cable_params['sigma']
-    rho   = cable_params['rho']
-    dwi_path  = f'./DWI_sigma_{sigma}_rho_{rho}.nii'
-    fod_path  = f'./FOD_sigma_{sigma}_rho_{rho}.nii'
-    tck_path  = f'tck.tck'
-    mask_path = f'mask.nii'
-    GradientWeightedFunction(img3d_path, cable_params, dwi_path)
-    ODFProcessing(dwi_path, cable_params, fod_path, mask_path, tck_path)
-    res_path_set = {
-        'dwi_path': dwi_path,
-        'fod_path': fod_path,
-        'tck_path': tck_path,
-        'mask_path': mask_path,
-    }
-    return res_path_set
-
 def main():
-    img3d_path = sys.argv[1]
-    cable_params = {
-        # smooth parameter for gradient
-        'sigma': 1,
-        # size of analyze step (also control analyze window and block size)
-        'rho'  : 20,
-        # downsample ratio, i.e. step size for gradient computaion
-        'ratio': 2,
-        'n_tck_sample': 10000,
-        'dir_path': './45.txt',
-        'response_path': './response.nii',
-    }
-    res_path_set = ComputeCABLE(img3d_path, cable_params)
-    # visulize the results
-    dwi_path = res_path_set['dwi_path']
-    tck_path = res_path_set['tck_path']
-    os.system(f'mrview "{dwi_path}" -imagevisible false -tractography.load "{tck_path}"')
+    address = sys.argv[1]
+    h5 = h5py.File(         address,        'r')
+    img = h5['DataSet']['ResolutionLevel 2']['TimePoint 0']['Channel 1']['Data']
+    ims_roi = [0,img.shape[0],
+               0,img.shape[1],
+               0,img.shape[2]]
+    nii_roi = [img.shape[2]-ims_roi[5], img.shape[2]-ims_roi[4],
+               img.shape[0]-ims_roi[1] , img.shape[0]-ims_roi[0],
+               img.shape[1]-ims_roi[3], img.shape[1]-ims_roi[2]]
+    ODF_out = np.zeros([(nii_roi[1]-nii_roi[0]) // rho,
+                      (nii_roi[3]-nii_roi[2]) // rho,
+                      (nii_roi[5]-nii_roi[4]) // rho, 46], dtype=np.float32)
+    ODF_partition = partition([10 * rho, 10 * rho, 10 * rho], nii_roi, rho)
+    dataSet = dataLoader.CustomIMSDataset(ODF_partition,address)
+    dataloader = dataLoader.DataLoader(dataSet,batch_size=1,num_workers=11,pin_memory=True)
+    k = torch.ones(rho//ratio).cuda(0)/(rho//ratio)
+    for batch in tqdm(dataloader, colour = 'GREEN'):
+        block = cp.asarray(batch[0][0])
+        Vx, Vy, Vz = grad(block)
+        d = cp.stack(( Vx, Vy, Vz), axis=-1)
+        x = psf(d,100)
+        x = torch.as_tensor(x[None, ...].transpose([4, 0, 1, 2, 3])).cuda()
+        x = F.conv3d(x, k[None, None, None, None, :], stride=[1, 1, rho // ratio])
+        x = F.conv3d(x, k[None, None, None, :, None], stride=[1, rho // ratio, 1])
+        x = F.conv3d(x, k[None, None, :, None, None], stride=[rho // ratio, 1, 1])
+        x = cp.array(x[..., None].transpose(0, -1)[0][0])
+        cp.nan_to_num(x, 0)
+        x = cp.concatenate([cp.max(x[:], axis=-1)[..., None] * 2, x], axis=-1)
+        x[...,0] = cp.where(x[...,0]>0.2,1,0)
+        x = x.get()
+        ODF_block = batch[1]
+        ODF_out[ (ODF_block[0][0]-nii_roi[0]) // rho:(ODF_block[0][1]-nii_roi[0]) // rho,(ODF_block[1][0]-nii_roi[2]) // rho:(ODF_block[1][1]-nii_roi[2]) // rho,(ODF_block[2][0]-nii_roi[4]) // rho:(ODF_block[2][1]-nii_roi[4]) // rho] = x
+    out = makeNiiForMRView(rho, ODF_out)
+    nib.save(out, dwi)
+
+    os.system(f'mrfilter  {dwi} smooth -stdev 0.04,0.04,0.04 {dwi}   -force')
+    os.system(f'dwi2fod msmt_csd {dwi} {res} {fod} -grad {dir_file} -lmax 8 -shells 1000 -force')
+    os.system(f'dwi2mask {dwi} mask.nii -grad {dir_file} -force')
+    os.system(f'tckgen -seed_image mask.nii {fod} tck.tck -select 20000 -power 2 -cutoff 0.05 -force -minlength 0.1 -maxlength 100000 -force')
+    os.system(f'mrview  {dwi} -imagevisible false -tractography.load tck.tck -tractography.opacity 0.5')
 
 if __name__ == '__main__':
     main()
